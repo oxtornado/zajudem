c817d87905e94e628b58ea6f2a6e9521a9506c18
oxtornado <dnielussa@gmail.com>
2025-04-01 15:47:56 -0500

    Import styles and update font family in button component; add Google Fonts to styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,5 @@
+import "../styles.css";
+
 const Button = () => {
   return (
     <button className="px-4 py-2 text-lg font-light text-black bg-white border border-gray-300 rounded-2xl shadow-md hover:shadow-lg active:shadow-inner transition-all">
