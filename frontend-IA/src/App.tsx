3a78e4e84344db53a2e1e90ef181906640329d5c
oxtornado <dnielussa@gmail.com>
2025-04-15 21:26:47 -0500

    agregando la página de inicio al enrutamiento

--- frontend-IA/src/App.tsx
+++ frontend-IA/src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Lock, Cloud } from 'lucide-react';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Inventory from './pages/Inventory';
 import Loans from './pages/Loans';
 import Layout from './components/Layout';
+import Hero from './pages/Hero';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Hero />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<Layout />}>
