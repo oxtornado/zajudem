9368f0b8c7ee2fabeb7dbec2b9551039233759ab
oxtornado <dnielussa@gmail.com>
2025-04-21 16:51:13 -0500

    re organized unnecesary  files

--- src/components/greenGradientText.tsx
+++ src/components/greenGradientText.tsx
@@ -1,6 +1,6 @@
 const GreenGradientText = () => {
   return (
-    <div className="absolute top-[750px] left-[550px] ">
+    <div className="absolute top-[800px] left-[550px] ">
       <div className="flex flex-col items-center justify-center">
         <p className="text-[30px] leading-[37px] font-instrument-sans font-bold">
           Nos aseguramos de tu{' '}
